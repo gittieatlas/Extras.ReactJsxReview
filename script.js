1dc215bd91ec96133498b5cdd1737bb5204a95a3
Gittie Atlas <gittieatlas@gmail.com>
2018-08-09 22:58:04 -0400

    React create element

---
+++ script.js
@@ -0,0 +1,9 @@
+// three args: element type, element attributes, children
+// this is the same as JSX of <h1 id="header">Hello World</h1>
+// under the hood, webpack is translating JSX into the below line
+const h1 = React.createElement('h1', { id: 'header' }, 'Hello World');
+
+const div = React.createElement('div', { id: 'wrapper' }, [h1]);
+
+// element to render, the node to append it to
+ReactDOM.render(div, document.getElementById('app'));
