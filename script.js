598a68dc73b11342dc7485fbae7c1d0c4b913692
Gittie Atlas <gittieatlas@gmail.com>
2018-08-09 23:02:28 -0400

    switch to JSX

--- script.js
+++ script.js
@@ -4,11 +4,13 @@
 const h1 = React.createElement('h1', { id: 'header' }, 'Hello World');
 
 const FirstComponent = () => {
-  return React.createElement('div', { id: 'wrapper' }, [h1]);
+  return (
+    <div id="wrapper">
+      <h1 id="header">Hello World</h1>
+    </div>
+  );
+  // return React.createElement('div', { id: 'wrapper' }, [h1]);
 };
 
 // element to render, the node to append it to
-ReactDOM.render(
-  React.createContext(FirstComponent),
-  document.getElementById('app')
-);
+ReactDOM.render(<FirstComponent />, document.getElementById('app'));
